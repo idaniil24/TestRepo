e656c12b0c0081ed33c46b301221ad25bb3371b8
Daniil Ignatev <i_daniil24@mail.ru>
2021-07-01 19:51:47 +0600

    TxtButton edition

--- app/src/main/java/com/example/testproject/MainActivity.java
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.testproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txtV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtV = (TextView) findViewById(R.id.txtV);
+        txtV.setText("Ваня пидар");
     }
 }
